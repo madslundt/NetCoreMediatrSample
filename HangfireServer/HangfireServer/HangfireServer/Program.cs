f633f8ab7c69cb1f808cd7307954dee7b93b9d5a
Mads Lundt <madslundt@live.dk>
2018-09-09 23:27:14 +0200

    Solution to additional Hangfire servers

---
+++ HangfireServer/HangfireServer/HangfireServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Hangfire;
+using System;
+
+namespace HangfireServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONNECTION_STRING = "";
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(CONNECTION_STRING);
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started.");
+                Console.WriteLine($"{nameof(Environment.MachineName)}: {Environment.MachineName}");
+                Console.WriteLine("\nPress any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
